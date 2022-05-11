ce50682be5d3e80d725cc1d736daccdd519ba319
Erwin Boskma <erwin@datarift.nl>
2022-05-11 14:36:22 +0200

    Cleanup convert.sh

--- convert.sh
+++ convert.sh
@@ -1,33 +1,94 @@
-rm -r outputs
-mkdir -p outputs
-cd outputs
+#!/usr/bin/env bash
 
-for name in ../inputs/*.ani; do
-    name=$(basename $name .ani)
-    mkdir -p $name
-    cd $name
-    cp ../../inputs/$name.ani .
-    ../../ani2ico/ani2ico $name.ani
-    rm $name.ani
-    for f in $name*.ico; do
-        filename=$(basename "$f")
-        png="${filename%.*}".png
-        convert $f $png
-        identify -format '%w 1 1 %f 200\n' $png >> $name.xcg
+set -e
+set -u
+set -o pipefail
+
+function usage() {
+    echo "cursor-converter"
+    echo ""
+    echo "Usage:"
+    echo "  cursor-converter <INPUT> <OUTPUT>"
+    echo ""
+    echo "Arguments:"
+    echo "  INPUT:   Directory containing the Windows cursors"
+    echo "  OUTPUT:  Directory to put the X cursors in"
+}
+
+function check_params() {
+    input="${1}"
+    output="${2}"
+
+    if [[ ! -d "${input}" ]]; then
+        echo "Input '${input}' is not a directory"
+        exit 2
+    fi
+
+    if [[ ! -d "${output}" ]]; then
+        mkdir -p "${output}"
+    fi
+}
+
+function convert_ani() {
+    echo "Converting .ani cursors"
+    input="${1}"
+    output="${2}"
+
+    for cursor in "${input}"/*.ani; do
+        cursor_name="$(basename "${cursor// /_}" .ani)"
+        
+        mkdir -p "${output}/${cursor_name}"
+        cp "${cursor}" "${output}/${cursor_name}/${cursor_name}.ani"
+        pushd "${output}/${cursor_name}" > /dev/null
+        ani2ico "${cursor_name}.ani"
+        rm "${cursor_name}.ani"
+
+        for ico in "${cursor_name}"*.ico; do
+            ico_name="$(basename "${ico}" .ico)"
+            convert "${ico}" "${ico_name}.png"
+            identify -format '%w 1 1 %f 200\n' "${ico_name}"*.png >> "${cursor_name}.xcg"
+        done
+
+        xcursorgen "${cursor_name}.xcg" "${cursor_name}"
+
+        rm ./*.ico ./*.png "${cursor_name}.xcg"
+        
+        popd > /dev/null
     done
-    xcursorgen $name.xcg $name
-    cd ..
-done
+}
 
-for name in ../inputs/*.cur; do
-    name=$(basename $name .cur)
-    mkdir -p $name
-    cd $name
-    cp ../../inputs/$name.cur .
-    convert $name.cur $name.png
-    identify -format '%w 1 1 %f\n' $name.png >> $name.xcg
-    xcursorgen $name.xcg $name
-    cd ..
-done
+function convert_cur() {
+    echo "Converting .cur cursors"
+    input="${1}"
+    output="${2}"
 
-cd ..
+    for cursor in "${input}"/*.cur; do
+        cursor_name="$(basename "${cursor// /_}" .cur)"
+
+        mkdir -p "${output}/${cursor_name}"
+
+        pushd "${output}/${cursor_name}" > /dev/null
+        convert "${cursor}" "${cursor_name}.png"
+        identify -format '%w 1 1 %f\n' "${cursor_name}"*.png > "${cursor_name}.xcg"
+        xcursorgen "${cursor_name}.xcg" "${cursor_name}"
+
+        rm ./*.png "${cursor_name}.xcg"
+        popd > /dev/null
+    done
+}
+
+function main() {
+    if [[ $# -ne 2 ]]; then
+        usage
+        exit 1
+    fi
+
+    echo "Converting cursors in '$1' and placing result in '$2'"
+
+    check_params "${1}" "${2}"
+    convert_ani "${1}" "${2}"
+    convert_cur "${1}" "${2}"
+}
+
+main "${@}"
+
